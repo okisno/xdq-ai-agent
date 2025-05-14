7bb33957d77f6565d97f0005be81e9ce80d27fae
xdq <3121627399@qq.com>
2025-05-14 22:45:03 +0800

    AI智能体初始化

---
+++ src/main/java/com/xdq/xdqaiagent/XdqAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.xdq.xdqaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XdqAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XdqAiAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xdq/xdqaiagent/XdqAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xdq.xdqaiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XdqAiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
